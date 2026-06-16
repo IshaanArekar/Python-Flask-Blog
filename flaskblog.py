8091a93cdb2af2eba1dcb8f7d7274828db412fdb
Ishaan Arekar <Ishaanarekar25@gmail.com>
2026-06-16 12:26:46 -0700

    Added template inheritance

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask , render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author' : 'Ishaan Arekar',
+        'title' : 'blog post 1',
+        'content' : 'blog post 1 content',
+        'date_posted' : '21st April, 2026'
+
+    },
+    {
+
+        'author' : 'test',
+        'title' : 'blog post 2',
+        'content' : 'blog post 2 content',
+        'date_posted' : '21st April, 2023'
+
+    }
+    ]
+
 
 @app.route("/")
 @app.route("/home")
-def Hello():
-    return "<h1>Hello World</h1>"
+def home():
+    return render_template('home.html', posts = posts)
+    
+
+
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':

4de219714bb6e2fa9effacae513fb5fbf958d7d9
Ishaan Arekar <Ishaanarekar25@gmail.com>
2026-06-23 10:00:49 -0700

    Restructure into package with models and routes

---
+++ flaskblog/routes.py
@@ -0,0 +1,50 @@
+from flaskblog.models import User,Post
+from flask import render_template, url_for, flash, redirect
+from flaskblog.forms import RegistrationForm, LogInForm
+from flaskblog import app
+posts = [
+    {
+        'author' : 'Ishaan Arekar',
+        'title' : 'Blog post 1',
+        'content' : 'Blog post 1 content',
+        'date_posted' : '21st April, 2026'
+
+    },
+    {
+
+        'author' : 'test',
+        'title' : 'Blog post 2',
+        'content' : 'Blog post 2 content',
+        'date_posted' : '21st April, 2023'
+
+    }
+    ]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}! You can now login", 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form , title = 'Register')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)

f29ce41dc465f19cee480033c24eeac21cb3d8dd
Ishaan Arekar <Ishaanarekar25@gmail.com>
2026-06-27 23:24:37 -0700

    Add password reset via email with tokenized links

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -60,7 +60,7 @@ class RequestResetForm(FlaskForm):
             if user is None:
                 raise ValidationError('There is no account with that email. You must register first.')
             
-class ResetPasswordForm:
+class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("Reset Password")
